eb12bd062e0d070258c0fa34a4f8ee14bb6d8252
Dhrubajyoti Bharali <dhrubajyotibharali88@gmail.com>
2022-02-14 20:36:11 +0530

    testing

---
+++ src/main/java/com/example/demo/Main.java
@@ -0,0 +1,47 @@
+package com.example.demo;
+
+public class Main {
+    String name;
+    int Age;
+    String addr;
+    char gender;
+
+    public Main(String name, int age, String addr, char gender) {
+        this.name = name;
+        Age = age;
+        this.addr = addr;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return Age;
+    }
+
+    public void setAge(int age) {
+        Age = age;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+}
